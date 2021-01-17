fee8ff3042a4ed6b833abf98b1699bda64d926af
WuphonsReach <wuphonsreach@gmail.com>
2021-01-17 06:28:57 -0500

    add personal rsync command to compile.sh

--- source/compile.sh
+++ source/compile.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 python2 compile.py
 
+rsync --checksum --recursive --verbose --exclude .git \
+    "../" \
+    "/home/syb/WR/mb/steam/steamapps/common/MountBlade Warband/Modules/Diplado-dev0006/"
+
