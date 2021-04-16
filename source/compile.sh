d338835f631d579fcbcfe17be5162457e642f91f
WuphonsReach <wuphonsreach@gmail.com>
2021-04-16 18:25:39 -0400

    Make compile.sh safer for other machines

--- source/compile.sh
+++ source/compile.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 python2 compile.py
 
-rsync --checksum --recursive --verbose --exclude .git \
-    "../" \
-    "/home/syb/WR/mb/steam/steamapps/common/MountBlade Warband/Modules/Diplado-dev0006/"
+# Adjust the following as needed based on where you keep your copy of Mount & Blade: Warband.
+# This directory must exist in order for it to be updated when you run this script.
+INSTALLDIR="/home/syb/WR/mb/steam/steamapps/common/MountBlade Warband/Modules/BeggarsDiplomacy"
 
+if [ -d "${INSTALLDIR}" ]; then
+  rsync --checksum --recursive --verbose --exclude .git \
+    "../" \
+    "${INSTALLDIR}/"
+fi
