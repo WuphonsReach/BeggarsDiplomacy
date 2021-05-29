d391162ec17ff9c5656c54e9461c5cf85fc11000
WuphonsReach <wuphonsreach@gmail.com>
2021-05-29 15:45:22 -0400

    reformat notification_border_incident, add TODO

--- source/module_game_menus.py
+++ source/module_game_menus.py
@@ -17403,15 +17403,15 @@ also excluded.^^You can change some settings here freely.",
 			(party_get_slot, ":target_faction", ":acting_village", slot_center_original_faction),
 			(try_begin),
 				(this_or_next|eq, ":target_faction", ":acting_faction"),
-                              		(neg|faction_slot_eq, ":target_faction", slot_faction_state, sfs_active),
+        (neg|faction_slot_eq, ":target_faction", slot_faction_state, sfs_active),
 				(party_get_slot, ":target_faction", ":acting_village", slot_center_ex_faction),
 			(try_end),
 
-		    (str_store_party_name, s1, ":acting_village"),
-		    (str_store_faction_name, s3, ":acting_faction"),
-		    (str_store_faction_name, s4, ":target_faction"),
+      (str_store_party_name, s1, ":acting_village"),
+      (str_store_faction_name, s3, ":acting_faction"),
+      (str_store_faction_name, s4, ":target_faction"),
 			(faction_get_slot, ":target_leader", ":target_faction", slot_faction_leader),
-		    (str_store_troop_name, s5, ":target_leader"),
+      (str_store_troop_name, s5, ":target_leader"),
 
 			(str_store_string, s9, "str_local_notables_from_s1_a_village_claimed_by_the_s4_have_been_mistreated_by_their_overlords_from_the_s3_and_petition_s5_for_protection"),
 			(display_log_message, "@There has been an alleged border incident: {s9}"),
@@ -17419,7 +17419,7 @@ also excluded.^^You can change some settings here freely.",
 			(call_script, "script_add_log_entry", logent_border_incident_subjects_mistreated, ":acting_village", -1, -1, ":acting_faction"),
 
 
-      (else_try),
+    (else_try),
 			(store_faction_of_party, ":target_faction", ":target_village"),
 
 		    (str_store_party_name, s1, ":acting_village"),
@@ -17458,15 +17458,17 @@ also excluded.^^You can change some settings here freely.",
 	  (store_add, ":slot_provocation_days", ":acting_faction", slot_faction_provocation_days_with_factions_begin),
 	  (val_sub, ":slot_provocation_days", kingdoms_begin),
 	  (faction_set_slot, ":target_faction", ":slot_provocation_days", 30),
+    
+    #TODO: Decrease any treaty length by some small amount
 
-      ],
+  ],
+  [
+    ("continue",[],"Continue",
     [
-      ("continue",[],"Continue",
-       [
-	   (change_screen_return),
-        ]),
-     ]
-  ),
+	    (change_screen_return),
+    ]),
+  ]
+),
 
 
 
